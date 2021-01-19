3a83db655f33a7500cd9fc3c21c10bd38118f495
Ubuntu <ubuntu@ip-172-31-39-151.ec2.internal>
2021-01-19 17:43:41 +0000

    added dating.py

---
+++ dating.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python 3
+#
+# Created by: Dahrio Francois
+# Created on: December 2020
+# this program detects if you have the qualities to date their grandchild
+#     if you're rich, or good looking
+
+
+def main():
+    # this function confirms your status
+
+    # input
+    string = input("Which are you? Rich? Or Good Looking? : ")
+    print("")
+
+    # process & input
+    if string == ("Rich"):
+        print("You can date my grandchild.")
+
+    elif string == ("Good Looking"):
+        print("You can date my grandchild.")
+
+    else:
+        print("You can't date my grandchild.")
+
+
+if __name__ == "__main__":
+    main()
